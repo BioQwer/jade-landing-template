413fe743c81db8bd19c786b243675ff40db910e0
Caleb Briancesco <jcaleb4@hotmail.com>
2015-11-23 09:48:54 -0600

    Updating tasks logics

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css'),
 	stylus = require('gulp-stylus');
  
 // Get one .styl file and render 
 gulp.task('stylus', function () {
-	gulp.src(['./src/styles/base/defaults.styl', './src/styles/modules/*.styl'])
-    .pipe(stylus({
-        compress: true
-    }))
+	gulp.src(['./src/styles/**/*.styl'])
+    .pipe(stylus())
     .pipe(concat('styles.min.css'))
     .pipe(gulp.dest('./build/css/'));
 });
+
+gulp.task('stylus-min', function () {
+    gulp.src(['./src/styles/**/*.styl'])
+    .pipe(stylus())
+    .pipe(concat('styles.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./build/css/'));
+});
+
